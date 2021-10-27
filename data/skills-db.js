567bd275929b9f2e4892c3324d00db0cb74fe8c9
C_Seckman <Cassie.seckman@gmail.com>
2021-10-27 19:54:25 -0400

    fix vairables

---
+++ data/skills-db.js
@@ -0,0 +1,60 @@
+const skills = [
+  {text: 'Javascript', _id: 12345},
+  {text: 'CSS', _id: 12346},
+  {text: 'Node', _id: 12347},
+]
+
+const find = (conditions, callback) => {
+  // see if this works, if not, execute the code in the catch block
+  try {
+    // make sure that conditions is an object - if not throw a TypeError
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    // If the object is empty, return all the todos
+    if (Object.keys(conditions).length === 0) return callback(null, skills)
+	// deal with errors
+  } catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
+
+const findById = (id, callback) =>{
+  try {
+    const skill = skills.find(skill => skill._id === parseInt(id))
+    if (!skill) throw new Error ('No skill was found')
+    return callback(null, skill)
+  } catch (error) {
+    console.log(error)
+    return callback(error, null)
+  }
+}
+
+function create(skill, callback) {
+  // Add the id
+  skill._id = Date.now() % 1000000
+  // New todos wouldn't be done
+  skill.done = false
+  skills.push(skill)
+  return callback(null, skill)
+}
+
+function findByIdAndDelete(id, callback) {
+  try { 
+    // Find the index based on the _id of tskill object
+    const idx = skills.findIndex(skill => skill._id == parseInt(id))
+    const deletedSkill = skills.splice(idx, 1)
+    if (!deletedSkill.length ) throw new Error ('No skill was deleted')
+    return callback(null, deletedSkill[0])
+  } catch(error) {
+    return callback(error, null)
+  }
+}
+
+export { 
+	find,
+  findById,
+  create,
+  findByIdAndDelete
+}
